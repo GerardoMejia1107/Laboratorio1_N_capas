1fc1bf63e3367581acedcdb4efc8b546f9180570
GerardoMejia1107 <00104923@uca.edu.sv>
2026-04-22 12:17:59 -0600

    chore::tada: Initializing proyect

---
+++ src/main/java/com/gerardo/laboratorio1_evaluacion/Laboratorio1EvaluacionApplication.java
@@ -0,0 +1,13 @@
+package com.gerardo.laboratorio1_evaluacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio1EvaluacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Laboratorio1EvaluacionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gerardo/laboratorio1_evaluacion/Laboratorio1EvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerardo.laboratorio1_evaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorio1EvaluacionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
