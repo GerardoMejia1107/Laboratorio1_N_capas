11916fa1d716fe4fed4b27209e0d73d1f874cdaf
GerardoMejia1107 <00104923@uca.edu.sv>
2026-04-22 12:37:37 -0600

    feat: :rocket: Adding BOW model design

---
+++ src/main/java/com/gerardo/laboratorio1_evaluacion/BOW/domain/model/BOWModel.java
@@ -0,0 +1,23 @@
+package com.gerardo.laboratorio1_evaluacion.BOW.domain.model;
+
+import com.gerardo.laboratorio1_evaluacion.BOW.utils.Location;
+import com.gerardo.laboratorio1_evaluacion.BOW.utils.Status;
+import com.gerardo.laboratorio1_evaluacion.BOW.utils.VirusBase;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BOWModel {
+    private String name;
+    private VirusBase base;
+    private int hazardLevel;
+    private String weakPoint;
+    private Status currentStatus;
+    private Location lastLocation;
+    private Location nextExpectedLocation;
+}
